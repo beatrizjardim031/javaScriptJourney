ac41de9bb86e68eb24c6d1053bed1af7f385d1fc
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-01-28 20:01:37 -0600

    Add files via upload
    First codes in JS

---
+++ build-a-sentence-maker.js
@@ -0,0 +1,21 @@
+let adjective = "whimsical";
+let noun = "fairy";
+let verb = "itchy";
+let place = "meadow";
+let adjective2 = "serene";
+let noun2 = "tuna bread";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "curious";
+noun = "owl";
+verb = "exploring";
+place = "forest";
+adjective2 = "gentle";
+noun2 = "rats";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
+
+
---
+++ lab-javascript-trivia-bot.js
@@ -0,0 +1,26 @@
+
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Jerry";
+let botLocation = "Seattle";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days in 1995 by Brendan Eich.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was never meant to be a serious programming language at first.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " lets you change the type of a variable whenever you want.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
+
+
+
